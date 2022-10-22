bc99caf97c8ce3da5957991bfca6d0229560f36c
st106773 <polinaevseeva41060@gmail.com>
2022-10-22 15:47:03 +0300

    (=*-*=)

---
+++ 2022.10.17-Homework-4/Task1/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	long x = 0;
+	float cnt = 0;
+	std::cin >> x;
+
+	for (int i = 1; i * i <= x; ++i)
+	{
+		if (x % 1 == 0)
+		{
+			++cnt;
+		}
+		if (i * i == x)
+		{
+			cnt -= 0.5;
+		}
+	}
+	std::cout << cnt * 2;
+
+	return EXIT_SUCCESS;
+}
---
+++ 2022.10.17-Homework-4/Task3/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	long n = 0;
+	int c = 1;
+	int cnt = 1;
+
+	std::cin >> n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		std::cout << c << " ";
+		if (cnt == c)
+		{
+			c += 1;
+			cnt = 0;
+		}
+		++cnt;
+	}
+	return EXIT_SUCCESS;
+}
